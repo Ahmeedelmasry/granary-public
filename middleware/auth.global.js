3a16c9eaa9a47f9b6f7b63e6f4213cb2f8c9345e
unknown <ahmed.elmazry@gmail.com>
2024-03-10 23:42:44 +0200

    updates

--- middleware/auth.global.js
+++ middleware/auth.global.js
@@ -2,11 +2,11 @@ export default defineNuxtRouteMiddleware(async (to, from) => {
   const logger = useCookie("logger");
   if (!logger.value) {
     if (to.name != "signin") {
-      return useRouter().push({ name: "signin" });
+      return navigateTo({ name: "signin" });
     }
   } else {
     if (to.name == "signin") {
-      return useRouter().push({ name: "index" });
+      return navigateTo({ name: "index" });
     }
   }
 });
